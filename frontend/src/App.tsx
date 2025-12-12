f262b37b2cc0910a0b4019258f27eb29a35a89ad
Janu <janagi2368@gmail.com>
2025-12-12 15:07:56 +0530

    Initiated frontend folder with vite react, added (tailwind css, shadcn ui and clerk for user management)

---
+++ frontend/src/App.tsx
@@ -0,0 +1,18 @@
+import { Button } from "./components/ui/button"
+import { SignInButton, SignedIn, SignedOut, UserButton } from "@clerk/clerk-react"
+const App = () => {
+  return (
+    <header>
+      <SignedOut>
+        <SignInButton>
+          <Button>Sign In</Button>
+        </SignInButton>
+      </SignedOut>
+      <SignedIn>
+        <UserButton />
+      </SignedIn>
+    </header>
+  )
+}
+
+export default App
